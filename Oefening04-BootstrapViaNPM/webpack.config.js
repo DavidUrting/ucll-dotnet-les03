37492f600c9ff227ceff8915133a7f13721ee880
David Urting <david.urting@live.com>
2019-09-28 18:14:22 +0200

    Oefening04 toegevoegd

---
+++ Oefening04-BootstrapViaNPM/webpack.config.js
@@ -0,0 +1,18 @@
+﻿const path = require('path');
+
+// webpack.config.js 
+module.exports = { 
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'wwwroot'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }
+    ]
+  }
+};
