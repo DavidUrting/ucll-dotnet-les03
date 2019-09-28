719ab75f073bc513339b2c6273ece6bbc5192ed2
David Urting <david.urting@live.com>
2019-09-28 16:52:59 +0200

    webpack build pipeline toegevoegd + loader voor scss + site.scss.

---
+++ Oefening01-SASS/webpack.config.js
@@ -0,0 +1,20 @@
+﻿const path = require('path');
+
+// webpack.config.js 
+module.exports = { 
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'wwwroot'),
+    filename: 'main.js'
+  },
+  module: { 
+    rules: [{ 
+      test: /\.scss$/, 
+        use: [ 
+        "style-loader", // creates style nodes from JS strings 
+        "css-loader", // translates CSS into CommonJS 
+        "sass-loader" // compiles Sass to CSS, using Node Sass 
+       ] 
+    }] 
+  } 
+};
