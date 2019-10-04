ac054a12e0ea2dfe8767548fbbe5c247729f1390
David Urting <david.urting@live.com>
2019-10-04 22:15:05 +0200

    vereenvoudiging webpack.config.js

--- Oefening01-SASS/webpack.config.js
+++ Oefening01-SASS/webpack.config.js
@@ -1,20 +1,19 @@
-﻿const path = require('path');
-
-// webpack.config.js 
-module.exports = { 
-  entry: { main: './src/index.js' },
-  output: {
-    path: path.resolve(__dirname, 'wwwroot'),
-    filename: 'main.js'
-  },
-  module: { 
-    rules: [{ 
-      test: /\.scss$/, 
-        use: [ 
-        "style-loader", // creates style nodes from JS strings 
-        "css-loader", // translates CSS into CommonJS 
-        "sass-loader" // compiles Sass to CSS, using Node Sass 
-       ] 
-    }] 
-  } 
+﻿// webpack.config.js 
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: '../wwwroot/main.js'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings 
+                "css-loader", // translates CSS into CommonJS 
+                "sass-loader" // compiles Sass to CSS, using Node Sass 
+            ]
+        }]
+    }
 };
